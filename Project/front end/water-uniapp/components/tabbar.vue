e8bd16ced9a5d7ce5287a76184aa7fdb5da29ce6
CK <ckisaboy@163.com>
2022-04-26 00:02:50 +0800

    init uniapp project

---
+++ Project/front end/water-uniapp/components/tabbar.vue
@@ -0,0 +1,38 @@
+<template>
+	<u-tabbar
+		value="value6"
+		@change="changeTab"
+		:fixed="true"
+		:placeholder="true"
+		:safeAreaInsetBottom="true"
+	 >
+	<!-- <view >
+		<u-tabbar-item text="{{item.text}}" icon="{{item.icon}}"></u-tabbar-item>
+	</view> -->
+	<u-tabbar-item v-for="item in menus" :text="item.name" :icon="item.icon">
+	</u-tabbar-item>
+	</u-tabbar>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				value6: '123',
+				menus: [
+					{text:'首页', icon:'home', selectIcon:'home-fill', path:''},
+					{text:'历史订单', icon:'home', selectIcon: '', path:''}
+				]
+			}
+		},
+		methods:{
+			changeTab(index) {
+				console.log(index);
+				console.log(this.menus)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ Project/front end/water-uniapp/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<tabbar></tabbar>
+</template>
+
+<script>
+	import tabbar from '../../components/tabbar.vue'
+	export default {
+		components: {tabbar},
+		data() {
+			return {
+				title: 'Hello',
+				value6: '123'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
