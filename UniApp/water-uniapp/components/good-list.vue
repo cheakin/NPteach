53bad3f005a994cea0866ef0030b2ec880d037a8
botboy <ckisaboy@163.com>
2022-05-20 00:26:57 +0800

    add: good page add:good-list component

---
+++ UniApp/water-uniapp/components/good-list.vue
@@ -0,0 +1,100 @@
+<template>
+	<view class="goods">
+		<view class="good_item" v-for="good in goods" :key="good.id">
+			<image :src="good.img_url" />
+			<view class="good_name">{{good.title}}</view>
+			<view class="good_desc">
+				<u-tag text="酒水" size="mini" color="#909399" borderColor="#fff" plain shape="circle" />
+				<view class="good_spec">{{good.spec}}</view>
+				
+			</view>
+			<view class="good_desc">
+				<view class="good_price">
+					<text>￥{{good.sell_price}}</text>
+					<text>￥{{good.market_price}}</text>
+				</view>
+				<u-tag text="+" size="mini" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"good-list",
+		props:[
+			'goods'
+		],
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods {
+		// border: red 1rpx solid;
+		padding: 0 15rpx;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+	
+		.good_item {
+			background-color: #fff;
+			width: 355rpx;
+			margin: 3rpx 0;
+			padding: 15rpx;
+			box-sizing: border-box;
+	
+			image {
+				width: 335rpx;
+				height: 300rpx;
+				// height: 250rpx;
+				display: block;
+				margin: auto;
+			}
+	
+			.good_name {
+				font-size: 36rpx;
+				line-height: 50rpx;
+				margin-top: 5rpx;
+				font-weight: bold;
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				// padding: 15rpx 0 10rpx 0;
+			}
+	
+			.good_desc {
+				// border: red 1rpx solid;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin-top: -1rpx;
+			}
+	
+			.good_spec {
+				font-size: 26rpx;
+				color: $u-info;
+			}
+	
+			.good_price {
+				// border: red 1rpx solid;
+				// position: relative;
+				// float: right;
+				// margin: 20rpx 0 5rpx 0;
+				color: $u-error-dark;
+				font-size: 45rpx;
+				text:nth-child(2) {
+					color: #82848a;
+					font-size: 18rpx;
+					margin-left: 17rpx;
+					text-decoration: line-through;
+				}
+			}
+		}
+	}
+
+</style>
---
+++ UniApp/water-uniapp/pages/basics/goods/goods.vue
@@ -0,0 +1,97 @@
+<template>
+	<view>
+		<u-sticky bgColor="#fff">
+			<u-tabs :list="list1" lineWidth="30" lineColor="#f56c6c" 
+			:activeStyle="{
+				color: '#303133',
+				fontWeight: 'bold',
+				transform: 'scale(1.05)'
+			}" 
+			:inactiveStyle="{
+				color: '#606266',
+				transform: 'scale(1)'
+			}" 
+			itemStyle="padding-left: 15px; padding-right: 15px; height: 34px;" />
+		</u-sticky>
+		<view style="margin-top: 200;">
+			<good-list :goods="goods" />
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import goodList from '@/components/good-list.vue'
+	export default {
+		components: {
+			"good-list": goodList
+		},
+		data() {
+			return {
+				pageParam: {
+					pageIndex: 1,
+					pageSize: 10
+				},
+				list1: [{
+					name: '关注',
+				}, {
+					name: '推荐',
+				}, {
+					name: '电影'
+				}, {
+					name: '科技'
+				}, {
+					name: '音乐'
+				}, {
+					name: '美食'
+				}, {
+					name: '文化'
+				}, {
+					name: '财经'
+				}, {
+					name: '手工'
+				}],
+				goods: [{
+						id: '1',
+						img_url: 'https://picsum.photos/id/16/100/100',
+						sell_price: 200,
+						market_price: 300,
+						title: '测试测试测试测试测试测试测试测试测试测试测试测试测试',
+						spec: '200ml X 1'
+					},
+					{
+						id: '2',
+						img_url: 'https://picsum.photos/id/16/200/200',
+						sell_price: 200,
+						market_price: 300,
+						title: '测试'
+					}, {
+						id: '3',
+						img_url: 'https://picsum.photos/id/16/200/200',
+						sell_price: 200,
+						market_price: 300,
+						title: '测试'
+					}, {
+						id: '4',
+						img_url: 'https://picsum.photos/id/16/200/200',
+						sell_price: 200,
+						market_price: 300,
+						title: '测试'
+					}
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			click(item) {
+				console.log('item', item);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
