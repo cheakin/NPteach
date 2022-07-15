6e22bbca037d3ccc12aa96f407f9df60a4df8eba
botboy <ckisaboy@163.com>
2022-07-16 02:07:21 +0800

    finish generate gulimall_product code

--- SpringCloud/gulimall/gulimall-product/src/main/java/cn/cheakin/gulimall/product/GulimallProductApplication.java
+++ SpringCloud/gulimall/gulimall-product/src/main/java/cn/cheakin/gulimall/product/GulimallProductApplication.java
@@ -1,11 +1,8 @@
 package cn.cheakin.gulimall.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//@MapperScan
 @SpringBootApplication
 public class GulimallProductApplication {
 
