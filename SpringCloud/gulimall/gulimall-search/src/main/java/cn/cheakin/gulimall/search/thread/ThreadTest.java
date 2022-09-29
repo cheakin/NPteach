37a594267d3a8b59b6e608f96e38e35a161dbced
botboy <ckisaboy@163.com>
2022-09-29 23:36:25 +0800

    add:gulimall-异步-线程池详解

--- SpringCloud/gulimall/gulimall-search/src/main/java/cn/cheakin/gulimall/search/thread/ThreadTest.java
+++ SpringCloud/gulimall/gulimall-search/src/main/java/cn/cheakin/gulimall/search/thread/ThreadTest.java
@@ -27,9 +27,9 @@ public class ThreadTest {
         // 阻塞等待整个线程执行完成, 获取返回结果
         System.out.println(futureTask.get());*/
 
-        // 我们以后在业务代码里面, 以上三松启动线程的方式都不用【将所有的线程异步任务都交给线程池执行】
+        // 我们以后在业务代码里面, 以上三种启动线程的方式都不用【将所有的线程异步任务都交给线程池执行】
 
-        // 4.使用线程池
+        // 4.使用线程池[ExecutorService]
         executor.execute(new Runable01());
 
         /**
@@ -38,6 +38,52 @@ public class ThreadTest {
          * 1，2，3都不能控制资源
          * 4可以控制资源，性能稳定。
          */
+
+
+        // 系统中池应该只有一二三个，每个一部任务，提交给线程池让他自己去执行就行
+        /**
+         * 七大参数
+         * corePoolSize:[5]核心线程书[一直存在除非(allowCoreThreadTimeOut];线程池，创建好以后就准备就绪的的线程数量，就等待来接受任务
+         *      5个  Thread thred  = new Thred();    thread.start();
+         * maximumPoolSize:[200] 最大线程数量；控制资源
+         * keepAliveTime:存活时间， 如果当前线程数量大于核心数量
+         *      释放空闲的线程(maximumPoolSize-corePoolSize)，只要线程空闲大于指定的keepAliveTime；
+         * unit:时间单位
+         * BlockingQueue<Runnable>：阻塞队列。如果任务有很多，就会将目前多的任务放在队列里面。
+         *      只要有线程空闲，就会去队列里面取出新的任务执行
+         * threadFactory：线程的创建工厂。
+         * RejectedExecutionHandler handler：如果队列满了，按照我们指定的拒绝策略拒绝执行任务
+         *
+         * 工作顺序
+         * 1、线程池创建，准备好 core 数量的核心线程，准备接受任务
+         *  (1) 、core 满了，就将再进来的任务放入阻塞队列中。空闲的 core 就会自己去阻塞队列获取任务执行
+         *  (2) 、阻塞队列满了，就直接开新线程执行，最大只能开到 max 指定的数量
+         *  (3) 、max满了就用RejectedExecutionHandler拒绝任务
+         *  (4) 、max都执行完成，有很多空闲，在指定的时间 keepAliveTime 以后，释放 max-core这些线程
+         * 3、所有的线程创建都是由指定的 factory 创建的。
+         *
+         *      new LinkedBlockingDeque<>(), 默认是Integer的最大值。内存会不够
+         *
+         *
+         *
+         * 面试：
+         * Q: 一个线程池 core 7； ； max 20  ，queue ：50 ，100 并发进来怎么分配的；
+         * A: 先有 7 个能直接得到执行，接下来 50 个进入队列排队，在多开 13 个继续执行。现在 70 个被安排上了。剩下 30 个使用拒绝策略。
+         */
+        // 线程池方式一
+        /*ThreadPoolExecutor executor = new ThreadPoolExecutor(5,
+                200,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(10000),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());*/
+        // 线程池方式二：
+        Executors.newCachedThreadPool();    // core是0，所有都可回收
+        Executors.newFixedThreadPool(5);     // 固定大小,core=max:都不可回收
+        Executors.newScheduledThreadPool();     // 定时任务的线程池
+        Executors.newSingleThreadExecutor();    // 单线程的线程池, 后台从队列中获取任务, 依次执行
+
 
         //Future<Integer> submit = executor.submit(new Callable01());
         //submit.get();
