27a071735337bcb51fe4f5085a5a3688ce90d75c
botboy <ckisaboy@163.com>
2023-01-30 17:34:06 +0800

    add:新增注册接口

--- SpringCloud/gulimall/gulimall-auth-server/src/main/java/cn/cheakin/gulimall/auth/controller/LoginController.java
+++ SpringCloud/gulimall/gulimall-auth-server/src/main/java/cn/cheakin/gulimall/auth/controller/LoginController.java
@@ -3,16 +3,27 @@ package cn.cheakin.gulimall.auth.controller;
 import cn.cheakin.common.constant.AuthServerConstant;
 import cn.cheakin.common.exception.BizCodeEnum;
 import cn.cheakin.common.utils.R;
+import cn.cheakin.gulimall.auth.feign.MemberFeignService;
 import cn.cheakin.gulimall.auth.feign.ThirdPartFeignService;
+import cn.cheakin.gulimall.auth.vo.UserRegisterVo;
+import cn.hutool.core.lang.TypeReference;
 import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Create by botboy on 2022/10/07.
@@ -22,10 +33,11 @@ public class LoginController {
 
     @Autowired
     private ThirdPartFeignService thirdPartFeignService;
+    @Autowired
+    private MemberFeignService memberFeignService;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
-
 
     /**
      * 发送一个请求直接跳到一个页面。
@@ -40,7 +52,6 @@ public class LoginController {
     public String regPage() {
         return "reg";
     }*/
-
 
 
     @ResponseBody
@@ -70,6 +81,70 @@ public class LoginController {
         thirdPartFeignService.sendCode(phone, codeNum);
 
         return R.ok();
+    }
+
+
+    /**
+     * TODO: 重定向携带数据：利用session原理，将数据放在session中。
+     * TODO:只要跳转到下一个页面取出这个数据以后，session里面的数据就会删掉
+     * TODO：分布下session问题
+     * RedirectAttributes：重定向也可以保留数据，不会丢失
+     * 用户注册
+     *
+     * @return
+     */
+    @PostMapping(value = "/register")
+    public String register(@Valid UserRegisterVo vos, BindingResult result, RedirectAttributes attributes) {
+        //如果有错误回到注册页面
+        if (result.hasErrors()) {
+            Map<String, String> errors = result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            attributes.addFlashAttribute("errors", errors);
+            //效验出错回到注册页面
+            //return "redirect:reg.html";   // 用户注册时时发送的post请求，而路径映射转发默认时使用get方式
+            //转发的话用户在刷新页面时会重新提交请求
+            /*model.addAttribute("errors", errors);
+            return "reg"; */
+            //return "redirect:reg.html";   //这样会重定向到ip下
+            return "redirect:http://auth.gulimall.com/reg.html";
+        }
+
+        //1、效验验证码
+        String code = vos.getCode();
+
+        //获取存入Redis里的验证码
+        String redisCode = stringRedisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + vos.getPhone());
+        if (!StrUtil.isEmpty(redisCode)) {
+            //截取字符串
+            if (code.equals(redisCode.split("_")[0])) {
+                //删除验证码;令牌机制
+                stringRedisTemplate.delete(AuthServerConstant.SMS_CODE_CACHE_PREFIX + vos.getPhone());
+                //验证码通过，真正注册，调用远程服务进行注册
+                R register = memberFeignService.register(vos);
+                if (register.getCode() == 0) {
+                    //成功
+                    return "redirect:http://auth.gulimall.com/login.html";
+                } else {
+                    //失败
+                    Map<String, String> errors = new HashMap<>();
+                    errors.put("msg", register.getData("msg", new TypeReference<String>() {
+                    }));
+                    attributes.addFlashAttribute("errors", errors);
+                    return "redirect:http://auth.gulimall.com/reg.html";
+                }
+            } else {
+                //效验出错回到注册页面
+                Map<String, String> errors = new HashMap<>();
+                errors.put("code", "验证码错误");
+                attributes.addFlashAttribute("errors", errors);
+                return "redirect:http://auth.gulimall.com/reg.html";
+            }
+        } else {
+            //效验出错回到注册页面
+            Map<String, String> errors = new HashMap<>();
+            errors.put("code", "验证码错误");
+            attributes.addFlashAttribute("errors", errors);
+            return "redirect:http://auth.gulimall.com/reg.html";
+        }
     }
 
 }
---
+++ SpringCloud/gulimall/gulimall-auth-server/src/main/java/cn/cheakin/gulimall/auth/vo/UserLoginVo.java
@@ -0,0 +1,11 @@
+package cn.cheakin.gulimall.auth.vo;
+
+import lombok.Data;
+
+@Data
+public class UserLoginVo {
+
+    private String loginacct;
+
+    private String password;
+}
