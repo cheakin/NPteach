25f139efbdb6903189c8605f6589a1fd3257ee69
botboy <ckisaboy@163.com>
2023-02-10 00:10:21 +0800

    pref:规范项目启动类名

--- SpringCloud/gulimall/gulimall-test-sso-client/src/main/java/cn/cheakin/gulimall/ssoclient/GulimallTestSsoClientApplication.java
+++ SpringCloud/gulimall/gulimall-test-sso-client/src/main/java/cn/cheakin/gulimall/ssoclient/GulimallTestSsoClientApplication.java
@@ -1,13 +1,13 @@
-package cn.cheakin.gulimall;
+package cn.cheakin.gulimall.ssoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GulimallTestSsoServer {
+public class GulimallTestSsoClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallTestSsoServer.class, args);
+        SpringApplication.run(GulimallTestSsoClientApplication.class, args);
     }
 
 
--- SpringCloud/gulimall/gulimall-test-sso-server/src/main/java/cn/cheakin/gulimall/GulimallTestSsoServerApplication.java
+++ SpringCloud/gulimall/gulimall-test-sso-server/src/main/java/cn/cheakin/gulimall/GulimallTestSsoServerApplication.java
@@ -1,14 +1,13 @@
-package cn.cheakin.gulimall.ssoclient;
+package cn.cheakin.gulimall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GulimallTestSsoClient2 {
+public class GulimallTestSsoServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallTestSsoClient2.class, args);
+        SpringApplication.run(GulimallTestSsoServerApplication.class, args);
     }
-
 
 }
