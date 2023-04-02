85f157d9b0791227ae52976a965cf66cf9821f30
botboy <ckisaboy@163.com>
2023-04-02 18:16:01 +0800

    update: 商城业务-支付-订单列表页渲染完成->修复一些bug

--- SpringCloud/gulimall/gulimall-member/src/main/java/cn/cheakin/gulimall/member/config/MemberWebConfiguration.java
+++ SpringCloud/gulimall/gulimall-member/src/main/java/cn/cheakin/gulimall/member/config/MemberWebConfiguration.java
@@ -1,18 +1,15 @@
 package cn.cheakin.gulimall.member.config;
 
 import cn.cheakin.gulimall.member.interceptor.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MemberWebConfiguration implements WebMvcConfigurer {
-    @Autowired
-    LoginInterceptor loginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
     }
 }
--- SpringCloud/gulimall/gulimall-member/src/main/java/cn/cheakin/gulimall/member/web/MemberWebController.java
+++ SpringCloud/gulimall/gulimall-member/src/main/java/cn/cheakin/gulimall/member/web/MemberWebController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -21,7 +22,7 @@ public class MemberWebController {
     public String memberOrderPage(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
                                   Model model){
         //查出当前登录的用户的所有订单列表数据
-        Map<String, Object> page = orderFeignService.listWithItem(null);
+        Map<String, Object> page = new HashMap<>();
         page.put("pageNum", pageNum.toString());
         R r = orderFeignService.listWithItem(page);
         model.addAttribute("orders", r);
